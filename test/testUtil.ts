3d3c2555ebe80006c72099a348c9a509c00d12f4
Chris F <cfsolutions1001@gmail.com>
2023-04-27 08:04:04 -0400

    feat: fix compatibility with Capacitor 4

--- test/testUtil.ts
+++ test/testUtil.ts
@@ -3,6 +3,7 @@
 
 import child_process = require("child_process");
 import os = require("os");
+import fs = require("fs");
 import path = require("path");
 import Q = require("q");
 
@@ -20,19 +21,21 @@ export class TestUtil {
     public static CORE_TESTS_ONLY: string = "--core";
     public static PULL_FROM_NPM: string = "--npm";
     public static SETUP: string = "--setup";
-    
+
     public static templatePath = path.join(__dirname, "../../test/template");
-    
+
     public static thisPluginPath = path.join(__dirname, "../..");
-    
+
+    public static defaultShouldUseWkWebView = 0;
+
     public static defaultAndroidServerUrl = "http://10.0.2.2:3001";
     public static defaultIOSServerUrl = "http://127.0.0.1:3000";
-    
+
     public static defaultAndroidEmulator = "emulator";
-    
-    private static defaultTestRunDirectory = path.join(os.tmpdir(), "cordova-plugin-code-push", "test-run");
-    private static defaultUpdatesDirectory = path.join(os.tmpdir(), "cordova-plugin-code-push", "updates");
-    
+
+    private static defaultTestRunDirectory = path.join(os.tmpdir(), "capacitor-code-push", "test-run");
+    private static defaultUpdatesDirectory = path.join(os.tmpdir(), "capacitor-code-push", "updates");
+
     /**
      * Reads the directory in which the test project is.
      */
@@ -42,7 +45,7 @@ export class TestUtil {
         console.log("testRunDirectory = " + testRunDirectory);
         return testRunDirectory;
     }
-    
+
     /**
      * Reads the directory in which the test project for updates is.
      */
@@ -52,17 +55,27 @@ export class TestUtil {
         console.log("testUpdatesDirectory = " + testUpdatesDirectory);
         return testUpdatesDirectory;
     }
-    
+
+    /**
+     * Reads wether or not whe should use WkWebView.
+     */
+    public static readShouldUseWkWebView(): number {
+        var commandLineOption = parseInt(TestUtil.readMochaCommandLineOption(TestUtil.SHOULD_USE_WKWEBVIEW));
+        var shouldUseWkWebView = commandLineOption ? commandLineOption : TestUtil.defaultShouldUseWkWebView;
+        console.log("shouldUseWkWebView = " + shouldUseWkWebView);
+        return shouldUseWkWebView;
+    }
+
     /**
      * Reads the path of the plugin (whether or not we should use the local copy or pull from npm)
      */
     public static readPluginPath(): string {
         var commandLineFlag = TestUtil.readMochaCommandLineFlag(TestUtil.PULL_FROM_NPM);
-        var pluginPath = commandLineFlag ? "cordova-plugin-code-push" : TestUtil.thisPluginPath;
+        var pluginPath = commandLineFlag ? "capacitor-code-push" : TestUtil.thisPluginPath;
         console.log("pluginPath = " + pluginPath);
         return pluginPath;
     }
-    
+
     /**
      * Reads the Android server url to use
      */
@@ -72,7 +85,7 @@ export class TestUtil {
         console.log("androidServerUrl = " + androidServerUrl);
         return androidServerUrl;
     }
-    
+
     /**
      * Reads the iOS server url to use
      */
@@ -81,27 +94,47 @@ export class TestUtil {
         var iOSServerUrl = commandLineOption ? commandLineOption : TestUtil.defaultIOSServerUrl;
         return iOSServerUrl;
     }
-    
+
     /**
      * Reads the Android emulator to use
      */
-    public static readAndroidEmulator(): string {
+    public static readAndroidEmulator(): Q.Promise<string> {
+        var deferred = Q.defer<string>();
+
+        function onReadAndroidEmuName(androidEmulatorName: string) {
+            console.log("Using " + androidEmulatorName + " for Android tests");
+            deferred.resolve(androidEmulatorName);
+        }
+
         var commandLineOption = TestUtil.readMochaCommandLineOption(TestUtil.ANDROID_EMULATOR);
-        var androidEmulator = commandLineOption ? commandLineOption : TestUtil.defaultAndroidEmulator;
-        return androidEmulator;
+        if (commandLineOption) {
+            onReadAndroidEmuName(commandLineOption);
+        } else {
+            // get the most recent iOS simulator to run tests on
+            this.getProcessOutput("emulator -list-avds")
+                .then(function (Devices) {
+                    const listOfDevices = Devices.trim().split("\n");
+                    onReadAndroidEmuName(listOfDevices[listOfDevices.length - 1]);
+                })
+                .catch((error) => {
+                    deferred.reject(error);
+                });
+        }
+
+        return deferred.promise;
     }
-    
+
     /**
      * Reads the iOS emulator to use
      */
     public static readIOSEmulator(): Q.Promise<string> {
         var deferred = Q.defer<string>();
-        
+
         function onReadIOSEmuName(iOSEmulatorName: string) {
             console.log("Using " + iOSEmulatorName + " for iOS tests");
             deferred.resolve(iOSEmulatorName);
         }
-            
+
         var commandLineOption = TestUtil.readMochaCommandLineOption(TestUtil.IOS_EMULATOR);
         if (commandLineOption) {
             onReadIOSEmuName(commandLineOption);
@@ -118,10 +151,10 @@ export class TestUtil {
                     deferred.reject(undefined);
                 });
         }
-                
+
         return deferred.promise;
     }
-    
+
     /**
      * Reads whether or not emulators should be restarted.
      */
@@ -130,7 +163,7 @@ export class TestUtil {
         if (restartEmulators) console.log("restart emulators");
         return restartEmulators;
     }
-    
+
     /**
      * Reads whether or not only core tests should be run.
      */
@@ -139,7 +172,33 @@ export class TestUtil {
         if (coreTestsOnly) console.log("only core tests");
         return coreTestsOnly;
     }
-    
+    /**
+     * Copies a file from a given location to another.
+     */
+    public static copyFile(source: string, destination: string, overwrite: boolean): Q.Promise<void>  {
+        var deferred = Q.defer<void>();
+        try {
+            console.log(`Copy ${source} to ${destination}`);
+
+            var errorHandler = function (error: Error) {
+                deferred.reject(error);
+            };
+            if (overwrite && fs.existsSync(destination)) {
+                fs.unlinkSync(destination);
+            }
+            var readStream = fs.createReadStream(source);
+            readStream.on("error", errorHandler);
+            var writeStream = fs.createWriteStream(destination);
+            writeStream.on("error", errorHandler);
+            writeStream.on("close", deferred.resolve.bind(undefined, undefined));
+            readStream.pipe(writeStream);
+        }
+        catch (e) {
+            deferred.reject(e);
+        }
+        return deferred.promise;
+    }
+
     /**
      * Reads whether or not to setup the test project directories.
      */
@@ -148,7 +207,7 @@ export class TestUtil {
         if (noSetup) console.log("set up test project directories");
         return noSetup;
     }
-    
+
     /**
      * Reads the test target platforms.
      */
@@ -163,25 +222,6 @@ export class TestUtil {
             platforms.push("ios");
         }
         return platforms;
-    }
-    
-    /**
-     * Reads if we should use the WkWebView or the UIWebView or run tests for both.
-     * 0 for UIWebView, 1 for WkWebView, 2 for both
-     */
-    public static readShouldUseWkWebView(): number {
-        var shouldUseWkWebView = TestUtil.readMochaCommandLineOption(TestUtil.SHOULD_USE_WKWEBVIEW);
-        switch (shouldUseWkWebView) {
-            case "true":
-                console.log("WkWebView");
-                return 1;
-            case "both":
-                console.log("Both WkWebView and UIWebView");
-                return 2;
-            case "false":
-            default:
-                return 0;
-        }
     }
 
     /**
@@ -213,7 +253,7 @@ export class TestUtil {
         }
         return false;
     }
-    
+
     /**
      * Executes a child process returns its output as a string.
      */
@@ -228,7 +268,14 @@ export class TestUtil {
             console.log("Running command: " + command);
         }
 
-        child_process.exec(command, options, (error: Error, stdout: Buffer, stderr: Buffer) => {
+        child_process.exec(
+            command,
+            options,
+            (
+                error: child_process.ExecException | null,
+                stdout: Buffer | string,
+                stderr: Buffer | string,
+            ) => {
 
             result += stdout;
 
